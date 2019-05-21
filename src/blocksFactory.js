979e9e619124e2524a75eaa0e104b82d731db86d
Andrey Martynov <andremartynov@gmail.com>
2019-05-21 17:30:57 +0300

    Added blocks factory

---
+++ src/blocksFactory.js
@@ -0,0 +1,67 @@
+let i = 0;
+
+const createBlock = (name, color, matrix) => ({
+  name,
+  i,
+  color,
+  matrix: matrix.map(r => r.map(v => v === 0 ? 0 : i++)),
+  x: 0,
+  y: 0
+});
+
+const blocks = [
+  createBlock(
+    'I',
+    '#00f0f0',
+    [
+      [ 1 ],
+      [ 1 ],
+      [ 1 ],
+    ]),
+  createBlock(
+    'J',
+    '#0000f0',
+    [
+      [ 0, 1 ],
+      [ 0, 1 ],
+      [ 1, 1 ],
+    ]),
+  createBlock(
+    'L',
+    '#f0a000',
+    [
+      [ 1, 0 ],
+      [ 1, 0 ],
+      [ 1, 1 ],
+    ]),
+  createBlock(
+    'O',
+    '#f0f000',
+    [
+      [ 1, 1 ],
+      [ 1, 1 ],
+    ]),
+  createBlock(
+    'S',
+    '#00f000',
+    [
+      [ 0, 1, 1 ],
+      [ 1, 1, 0 ],
+    ]),
+  createBlock(
+    'T',
+    '#a000f0',
+    [
+      [ 0, 1, 0 ],
+      [ 1, 1, 1 ],
+    ]),
+  createBlock(
+    'Z',
+    '#f00000',
+    [
+      [ 1, 1, 0 ],
+      [ 0, 1, 1 ],
+    ]),
+];
+
+export default blocks;
